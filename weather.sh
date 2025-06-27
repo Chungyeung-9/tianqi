17e83b81a62d534c0e422e1b35c2053149e7a05c
chungyeung li <321451972@qq.com>
2025-06-27 19:56:29 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -2,7 +2,9 @@
 
 set -eux
 
-CITY=Shanghai
+CITY1=Shanghai
+CITY2=Anhui
+CITY3=Nanjing
 LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
@@ -11,4 +13,6 @@ curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o result.html \
-  https://wttr.in/$CITY?format=4\&$UNIT
+  https://wttr.in/$CITY1?format=4\&$UNIT
+  https://wttr.in/$CITY2?format=4\&$UNIT
+  https://wttr.in/$CITY3?format=4\&$UNIT
